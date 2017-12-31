9939669a2d64d5833ba4e584b2d38f394b15168b
adelliu <lqj679ssn@qq.com>
2017-12-31 12:05:40 -0800

    优化字典

---
+++ Init/polish_phrase.py
@@ -0,0 +1,17 @@
+import codecs
+import os
+
+from Hiphop.settings import BASE_DIR
+
+CRT_PATH = os.path.join(BASE_DIR, 'Init')
+
+with open(os.path.join(CRT_PATH, 'phrase'), 'wb+') as f:
+    for s in codecs.open(os.path.join(CRT_PATH, 'phrase_raw'), 'r+', encoding='utf8'):
+        if s[-1] == '\n':
+            s = s[:-1]
+        flag = True
+        for c in s:
+            if c.upper() in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+                flag = False
+        if flag:
+            f.write((s+'\n').encode())
